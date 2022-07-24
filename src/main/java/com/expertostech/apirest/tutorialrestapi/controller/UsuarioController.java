9d78eb34a86859f944545f631e0e1074f5c974d3
ThaliaLevy <thalialevy@hotmail.com>
2022-07-23 22:05:44 -0300

    adicionando endpoint que lista todos os usuários salvos

--- src/main/java/com/expertostech/apirest/tutorialrestapi/controller/UsuarioController.java
+++ src/main/java/com/expertostech/apirest/tutorialrestapi/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.expertostech.apirest.tutorialrestapi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,23 +28,24 @@ public class UsuarioController {
 
 	@GetMapping(path = "/api/usuario/{codigo}")
 	public ResponseEntity consultarUsuario(@PathVariable("codigo") Integer codigo) {
+										// @RequestParam(value = "page", defaultValue = "1") int page, 
+										// @RequestParam(value = "sort", required = false) int sort,		//ordenar
+										// @RequestParam(value = "limit", defaultValue = "10") int limit) {	//limitar resultados
 		return usuarioRepository.findById(codigo)
 				.map(record -> ResponseEntity.ok().body(record))	//caso a requisição retorne algo, trará o resultado
 				.orElse(ResponseEntity.notFound().build());	//caso não haja resposta, retornará not found
 	}
 	
 	@GetMapping(path = "/api/usuario/listar")
-	public ResponseEntity<UsuarioRepository> listarUsuarios() {
-									// @RequestParam(value = "page", defaultValue = "1") int page, 
-									// @RequestParam(value = "sort", required = false) int sort,		//ordenar
-									// @RequestParam(value = "limit", defaultValue = "10") int limit) {	//limitar resultados
-		return new ResponseEntity<UsuarioRepository>(usuarioRepository, HttpStatus.OK); //(ResponseEntity) usuarioRepository.findAll();
+	public ResponseEntity<Iterable<UsuarioModel>> listarUsuarios() {		
+		Iterable<UsuarioModel> usuariosSalvos = usuarioRepository.findAll();
+		return new ResponseEntity<Iterable<UsuarioModel>>(usuariosSalvos, HttpStatus.OK); //
+		
 	}
 	
 	@PostMapping(path = "/api/usuario/salvar")
 	public UsuarioModel salvarUsuario(@RequestBody UsuarioModel usuario) {
 		return usuarioRepository.save(usuario);
-		
 	}
 	
 	//@PutMapping
